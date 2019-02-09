271ce942b7aef5416f506dfbf5a7c15bdb8679a3
jiangKlijna <leil729564090@outlook.com>
2019-02-09 22:04:52 +0800

    :cat:update

--- engine.go
+++ engine.go
@@ -6,6 +6,11 @@ import (
 
 var ancestor *otto.Otto = otto.New()
 
+var cache []*otto.Otto = make([]*otto.Otto, 100)
+
 func GetJsEngine() *otto.Otto {
+	if len(cache) == 0 {
+
+	}
 	return ancestor.Copy()
 }
