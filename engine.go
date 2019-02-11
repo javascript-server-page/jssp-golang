098f7082af22d7714bded3f3617e9353dba49d0d
jiangKlijna <leil729564090@outlook.com>
2019-02-11 21:43:34 +0800

    :cat:update

--- engine.go
+++ engine.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"container/list"
 	"github.com/robertkrimen/otto"
 )
 
 var ancestor *otto.Otto = otto.New()
 
-var cache []*otto.Otto = make([]*otto.Otto, 100)
+var cache *list.List = list.New()
 
 func NewJsEngine() *otto.Otto {
 	return ancestor.Copy()
@@ -14,7 +15,7 @@ func NewJsEngine() *otto.Otto {
 
 
 func GetJsEngine() *otto.Otto {
-	if len(cache) == 0 {
+	if cache.Len() == 0 {
 
 	}
 	return NewJsEngine()
