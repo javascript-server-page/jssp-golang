28b02ccd878b39e5a0ed3c2f4d0c7b5ede2ebe42
jiangKlijna <leil729564090@outlook.com>
2019-02-13 11:32:19 +0800

    :cat:cache engine

--- engine.go
+++ engine.go
@@ -5,9 +5,26 @@ import (
 	"github.com/robertkrimen/otto"
 )
 
+const cache_max = 500
+
 var ancestor *otto.Otto = otto.New()
 
 var cache *list.List = list.New()
+
+var isGenerate = make(chan bool)
+
+func init() {
+	go generate()
+}
+
+func generate() {
+	for {
+		<-isGenerate
+		for cache.Len() < cache_max {
+			cache.PushBack(NewJsEngine())
+		}
+	}
+}
 
 func NewJsEngine() *otto.Otto {
 	return ancestor.Copy()
@@ -15,7 +32,8 @@ func NewJsEngine() *otto.Otto {
 
 func GetJsEngine() *otto.Otto {
 	if cache.Len() == 0 {
+		isGenerate <- true
 		return NewJsEngine()
 	}
-	return cache.Front().Value.(*otto.Otto)
+	return cache.Remove(cache.Front()).(*otto.Otto)
 }
