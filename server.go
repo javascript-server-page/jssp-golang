7660fe02eaf28b2dc984d9071b76530ee043362f
jiangKlijna <leil729564090@outlook.com>
2019-02-20 20:02:22 +0800

    :cat:getJsAndExt

--- server.go
+++ server.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
-	"path/filepath"
+	"strings"
 )
 
 type JsspServer struct {
@@ -22,30 +22,42 @@ func (s *JsspServer) Init(paras *Parameter) {
 
 // handler func
 func (s *JsspServer) ServeAll(w http.ResponseWriter, r *http.Request) {
-	index, ext := s.getIndexAndExt(r.URL)
-	r.URL.Path += index
+	index, ext := s.getJsIndexAndExt(r.URL)
 	switch ext {
 	case "jssp":
-		s.ServeJssp(w, r)
+		s.ServeJssp(w, r, index)
 	case "jsjs":
-		s.ServeJsjs(w, r)
+		s.ServeJsjs(w, r, index)
 	default:
 		s.static.ServeHTTP(w, r)
 	}
 }
 
-func (s *JsspServer) ServeJssp(w http.ResponseWriter, r *http.Request) {
+func (s *JsspServer) ServeJssp(w http.ResponseWriter, r *http.Request, f *http.File) {
+	w.Write([]byte("jssp"))
 }
 
-func (s *JsspServer) ServeJsjs(w http.ResponseWriter, r *http.Request) {
+func (s *JsspServer) ServeJsjs(w http.ResponseWriter, r *http.Request, f *http.File) {
+	w.Write([]byte("jsjs"))
 }
 
-func (s *JsspServer) getIndexAndExt(u *url.URL) (string, string) {
+func (s *JsspServer) getJsIndexAndExt(u *url.URL) (*http.File, string) {
+	const JSSP = "index.jssp"
+	const JSJS = "index.jsjs"
 	if u.Path[len(u.Path)-1] == '/' {
-		return "", ""
-	} else {
-		return "", filepath.Ext(u.Path)
+		if !strings.HasPrefix(u.Path, "/") {
+			u.Path = "/" + u.Path
+		}
+		f := getFile(s.root, u.Path+JSSP)
+		if f != nil {
+			return f, "jssp"
+		}
+		f = getFile(s.root, u.Path+JSJS)
+		if f != nil {
+			return f, "jsjs"
+		}
 	}
+	return nil, ""
 }
 
 // run Jssp server
