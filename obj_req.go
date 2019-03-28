08c81950eaa2e3f84684b8f99365d76c35655b72
jiangKlijna <leil729564090@outlook.com>
2019-03-28 23:25:55 +0800

    :cat:finish req

--- obj_req.go
+++ obj_req.go
@@ -7,6 +7,11 @@ import (
 
 func GenerateObjReq(jse *JsEngine, r *http.Request) *otto.Object {
 	obj := jse.CreateObject()
+	obj.Set("header", build_header(jse, r.Header))
+	obj.Set("host", r.Host)
 	obj.Set("method", r.Method)
+	obj.Set("path", r.URL.Path)
+	obj.Set("proto", r.Proto)
+	obj.Set("remoteAddr", r.RemoteAddr)
 	return obj
 }
