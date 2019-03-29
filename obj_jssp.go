9fa0b289ad6dae1ef032fa5a53028e6d3fdd17a1
jiangKlijna <leil729564090@outlook.com>
2019-03-29 21:10:35 +0800

    :cat:add exec func

--- obj_jssp.go
+++ obj_jssp.go
@@ -2,12 +2,33 @@ package main
 
 import (
 	"github.com/robertkrimen/otto"
+	"os/exec"
 )
 
 func GenerateObjJssp(jse *JsEngine) *otto.Object {
 	obj := jse.CreateObject()
 	obj.Set("exec", func(call otto.FunctionCall) otto.Value {
-		return otto.Value{}
+		var res []byte
+		var err error
+		switch len(call.ArgumentList) {
+		case 0:
+			return otto.UndefinedValue()
+		case 1:
+			cmd := exec.Command(call.Argument(0).String())
+			res, err = cmd.CombinedOutput()
+		default:
+			ss := make([]string, 0)
+			for _, v := range call.ArgumentList {
+				ss = append(ss, v.String())
+			}
+			cmd := exec.Command(ss[0], ss[1:]...)
+			res, err = cmd.CombinedOutput()
+		}
+		if err != nil {
+			return *jse.CreateString(err.Error())
+		} else {
+			return *jse.CreateString(string(res))
+		}
 	})
 	return obj
 }
