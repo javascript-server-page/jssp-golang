47d114b4a4cfe904af774c023a3e6b7a2873910d
jiangKlijna <leil729564090@outlook.com>
2019-04-05 19:17:26 +0800

    :cat:adjust

--- obj_res.go
+++ obj_res.go
@@ -16,12 +16,5 @@ func GenerateObjRes(jse *JsEngine, w http.ResponseWriter) *otto.Object {
 	jse.Set("include", func(call otto.FunctionCall) otto.Value {
 		return otto.Value{}
 	})
-	obj.Set("print", func(call otto.FunctionCall) otto.Value {
-		if val := call.Argument(0); !val.IsUndefined() {
-			w.Write([]byte(val.String()))
-		}
-		w.Write([]byte("\n"))
-		return otto.Value{}
-	})
 	return obj
 }
