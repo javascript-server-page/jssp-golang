ba402203ddb0cbf7e2af78f2ecd332190050e7b8
jiangKlijna <leil729564090@outlook.com>
2019-04-06 19:06:14 +0800

    :cat:add func

--- obj_jsdo.go
+++ obj_jsdo.go
@@ -9,5 +9,11 @@ func GenerateObjJsdo(jse *JsEngine) *otto.Object {
 	obj.Set("mysql", func(call otto.FunctionCall) otto.Value {
 		return otto.Value{}
 	})
+	obj.Set("sqlserver", func(call otto.FunctionCall) otto.Value {
+		return otto.Value{}
+	})
+	obj.Set("postgres", func(call otto.FunctionCall) otto.Value {
+		return otto.Value{}
+	})
 	return obj
 }
