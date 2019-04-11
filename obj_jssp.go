9e40b5a43b0d59c10203730591f561f5e27392a1
jiangKlijna <leil729564090@outlook.com>
2019-04-11 19:31:19 +0800

    :cat:adjust

--- obj_jssp.go
+++ obj_jssp.go
@@ -9,30 +9,35 @@ import (
 func GenerateObjJssp(jse *JsEngine) *otto.Object {
 	obj := jse.CreateObject()
 	obj.Set("exec", func(call otto.FunctionCall) otto.Value {
-		var res []byte
-		var err error
-		switch len(call.ArgumentList) {
-		case 0:
+		if len(call.ArgumentList) == 0 {
 			return otto.UndefinedValue()
-		case 1:
-			cmd := exec.Command(call.Argument(0).String())
-			res, err = cmd.CombinedOutput()
-		default:
-			ss := make([]string, 0)
-			for _, v := range call.ArgumentList {
-				ss = append(ss, v.String())
-			}
-			cmd := exec.Command(ss[0], ss[1:]...)
-			res, err = cmd.CombinedOutput()
 		}
-		if err != nil {
-			return *jse.CreateString(err.Error())
-		} else {
-			return *jse.CreateString(string(res))
-		}
+		return *jse.CreateString(def_exec(call))
 	})
 	obj.Set("version", Server)
 	obj.Set("os", runtime.GOOS)
 	obj.Set("arch", runtime.GOARCH)
 	return obj
+}
+
+// execute system command line
+func def_exec(call otto.FunctionCall) string {
+	var res []byte
+	var err error
+	if len(call.ArgumentList) == 1 {
+		cmd := exec.Command(call.Argument(0).String())
+		res, err = cmd.CombinedOutput()
+	} else {
+		ss := make([]string, 0)
+		for _, v := range call.ArgumentList {
+			ss = append(ss, v.String())
+		}
+		cmd := exec.Command(ss[0], ss[1:]...)
+		res, err = cmd.CombinedOutput()
+	}
+	if err != nil {
+		return err.Error()
+	} else {
+		return string(res)
+	}
 }
