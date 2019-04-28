934ee10603dee1be002a96312f4797c5f0c1eae5
jiangKlijna <leil729564090@outlook.com>
2019-04-28 18:47:40 +0800

    :cat:rewrite include func

--- obj_res.go
+++ obj_res.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/robertkrimen/otto"
 	"net/http"
+	"strings"
 )
 
 func GenerateObjRes(jse *JsEngine, w http.ResponseWriter) *otto.Object {
@@ -19,7 +20,12 @@ func GenerateObjRes(jse *JsEngine, w http.ResponseWriter) *otto.Object {
 		if err != nil {
 			return *jse.CreateError(err)
 		}
-		f, err := file.Object().Call("open", call.Argument(0))
+		fval := call.Argument(0)
+		if fval.IsUndefined() {
+			return fval
+		}
+		fname := fval.String()
+		f, err := file.Object().Call("open", fname)
 		if err != nil {
 			return *jse.CreateError(err)
 		}
@@ -28,12 +34,25 @@ func GenerateObjRes(jse *JsEngine, w http.ResponseWriter) *otto.Object {
 		if err != nil {
 			return *jse.CreateError(err)
 		}
-		str, err := jse.Run(src.String())
-		if err != nil {
-			return *jse.CreateError(err)
+		if strings.HasSuffix(fname, "js") {
+			return *def_runsrc(jse, src.String())
+		} else if strings.HasSuffix(fname, ".jssp") {
+			src := jssp_jsjs([]byte(src.String()))
+			return *def_runsrc(jse, src)
 		} else {
-			return *jse.CreateString(str.String())
+			w.Write([]byte(src.String()))
+			return otto.UndefinedValue()
 		}
 	})
 	return obj
+}
+
+// run js src code
+func def_runsrc(jse *JsEngine, src interface{}) *otto.Value {
+	str, err := jse.Run(src)
+	if err != nil {
+		return jse.CreateError(err)
+	} else {
+		return jse.CreateString(str.String())
+	}
 }
