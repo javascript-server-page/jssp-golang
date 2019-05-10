74bef2e2af04ed7f1ceaffd0c00797282c2a2eec
jiang <leil729564090@outlook.com>
2019-05-10 16:37:44 +0800

    :cat:add req.parm func

--- obj_req.go
+++ obj_req.go
@@ -6,6 +6,7 @@ import (
 )
 
 func GenerateObjReq(jse *JsEngine, r *http.Request) *otto.Object {
+	r.ParseForm()
 	obj := jse.CreateObjectValue().Object()
 	obj.Set("header", build_header(jse, r.Header))
 	obj.Set("host", r.Host)
@@ -13,5 +14,13 @@ func GenerateObjReq(jse *JsEngine, r *http.Request) *otto.Object {
 	obj.Set("path", r.URL.Path)
 	obj.Set("proto", r.Proto)
 	obj.Set("remoteAddr", r.RemoteAddr)
+	obj.Set("parm", func(call otto.FunctionCall) otto.Value {
+		key := call.Argument(0)
+		if key.IsUndefined() {
+			return key
+		}
+		val := r.Form.Get(key.String())
+		return *jse.CreateAny(val)
+	})
 	return obj
 }
