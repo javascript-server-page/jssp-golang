e5293eebcb7c19b2936db5841f89c9cd80d8fd6c
jiangKlijna <leil729564090@outlook.com>
2019-05-13 20:05:34 +0800

    :cat:add req.session

--- obj_req.go
+++ obj_req.go
@@ -15,6 +15,7 @@ func GenerateObjReq(jse *JsEngine, r *http.Request) *otto.Object {
 	obj.Set("proto", r.Proto)
 	obj.Set("remoteAddr", r.RemoteAddr)
 	obj.Set("cookie", build_cookie(jse, r))
+	obj.Set("session", build_session(jse, r))
 	obj.Set("parm", func(call otto.FunctionCall) otto.Value {
 		key := call.Argument(0)
 		if key.IsUndefined() {
@@ -28,6 +29,45 @@ func GenerateObjReq(jse *JsEngine, r *http.Request) *otto.Object {
 
 // build req.cookie object
 func build_cookie(jse *JsEngine, r *http.Request) *otto.Object {
+	obj := jse.CreateObjectValue().Object()
+	obj.Set("get", func(call otto.FunctionCall) otto.Value {
+		val := call.Argument(0)
+		if val.IsUndefined() {
+			return val
+		}
+		c, err := r.Cookie(val.String())
+		if err != nil {
+			return otto.UndefinedValue()
+		}
+		return *jse.CreateAny(c.Value)
+	})
+	obj.Set("set", func(call otto.FunctionCall) otto.Value {
+		key := call.Argument(0)
+		if key.IsUndefined() || key.IsNull() {
+			return key
+		}
+		keystr := key.String()
+		val := call.Argument(1)
+		c := &http.Cookie{Name: keystr, Value: val.String(),}
+		if val.IsUndefined() || val.IsNull() {
+			c.MaxAge = -1
+		}
+		r.AddCookie(c)
+		return otto.UndefinedValue()
+	})
+	obj.Set("map", func(call otto.FunctionCall) otto.Value {
+		val := jse.CreateObjectValue()
+		obj := val.Object()
+		for _, k := range r.Cookies() {
+			obj.Set(k.Name, k.Value)
+		}
+		return *val
+	})
+	return obj
+}
+
+// build req.session object
+func build_session(jse *JsEngine, r *http.Request) *otto.Object {
 	obj := jse.CreateObjectValue().Object()
 	obj.Set("get", func(call otto.FunctionCall) otto.Value {
 		val := call.Argument(0)
