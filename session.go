cc9456c28959f0dc59f17626f6ebc441b60cefbc
jiang <leil729564090@outlook.com>
2019-05-27 17:36:36 +0800

    :cat:adjust

--- session.go
+++ session.go
@@ -1,16 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"github.com/robertkrimen/otto"
+	"net/http"
+	"sync"
+)
 
 const SESSION_KEY = "JSSP-SESSION-ID"
 
+var sessions_mutex = new(sync.Mutex)
 var sessions = make(map[string]*Session)
 
 type Session struct {
-	data map[string]string
+	id    string
+	mutex *sync.Mutex
+	data  map[string]*otto.Value
+}
+
+func NewSession(id string) *Session {
+	return &Session{id, new(sync.Mutex), make(map[string]*otto.Value)}
 }
 
 func GetSession(r *http.Request) *Session {
+	sessions_mutex.Lock()
+	defer sessions_mutex.Unlock()
 	c, err := r.Cookie(SESSION_KEY)
 	if err != nil {
 		c = &http.Cookie{Name: SESSION_KEY, Value: getUUID()}
@@ -18,7 +31,7 @@ func GetSession(r *http.Request) *Session {
 	}
 	s, ok := sessions[c.Value]
 	if !ok {
-		s = &Session{}
+		s = NewSession(c.Value)
 		sessions[c.Value] = s
 	}
 	return s
