cd421fdd3cdbc1acbb3aba2470e1018ab963e3c9
jiangKlijna <leil729564090@outlook.com>
2019-05-29 22:25:20 +0800

    :cat:adjust

--- session.go
+++ session.go
@@ -4,35 +4,43 @@ import (
 	"github.com/robertkrimen/otto"
 	"net/http"
 	"sync"
+	"time"
 )
 
 const SESSION_KEY = "JSSP-SESSION-ID"
 
-var sessions_mutex = new(sync.Mutex)
-var sessions = make(map[string]*Session)
-
-type Session struct {
-	id    string
+type Sessions struct {
 	mutex *sync.Mutex
-	data  map[string]*otto.Value
+	data  map[string]*Session
 }
 
-func NewSession(id string) *Session {
-	return &Session{id, new(sync.Mutex), make(map[string]*otto.Value)}
+func (ss *Sessions) NewSession(id string) *Session {
+	return &Session{id, time.Now(), new(sync.Mutex), make(map[string]*otto.Value)}
 }
 
-func GetSession(r *http.Request) *Session {
-	sessions_mutex.Lock()
-	defer sessions_mutex.Unlock()
+func (ss *Sessions) GetSession(r *http.Request) *Session {
+	ss.mutex.Lock()
+	defer ss.mutex.Unlock()
 	c, err := r.Cookie(SESSION_KEY)
 	if err != nil {
 		c = &http.Cookie{Name: SESSION_KEY, Value: getUUID()}
 		r.AddCookie(c)
 	}
-	s, ok := sessions[c.Value]
+	s, ok := ss.data[c.Value]
 	if !ok {
-		s = NewSession(c.Value)
-		sessions[c.Value] = s
+		s = ss.NewSession(c.Value)
+		ss.data[c.Value] = s
 	}
 	return s
+}
+
+type Session struct {
+	id    string
+	ct    time.Time
+	mutex *sync.Mutex
+	data  map[string]*otto.Value
+}
+
+func (s *Session) isExpired() bool {
+	return false
 }
