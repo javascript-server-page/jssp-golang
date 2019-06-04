48ff1376d6ad57bd12fa21d41e9a6fa70eb5a7f2
jiang <leil729564090@outlook.com>
2019-06-04 12:42:21 +0800

    :cat:def adjust

--- obj_file.go
+++ obj_file.go
@@ -11,135 +11,91 @@ import (
 func GenerateObjFile(js *JavaScript, jspath string) *otto.Object {
 	dir, _ := path.Split(jspath)
 	obj := js.CreateObjectValue().Object()
-	obj.Set("open", func(call otto.FunctionCall) otto.Value {
-		return *def_openfile(js, &call, dir, os.O_RDWR)
+	obj.Set("open", func(filename string) otto.Value {
+		return *def_openfile(js, filepath.Join(dir, filename), os.O_RDWR)
 	})
-	obj.Set("opena", func(call otto.FunctionCall) otto.Value {
-		return *def_openfile(js, &call, dir, os.O_RDWR|os.O_APPEND)
+	obj.Set("opena", func(filename string) otto.Value {
+		return *def_openfile(js, filepath.Join(dir, filename), os.O_RDWR|os.O_APPEND)
 	})
-	obj.Set("create", func(call otto.FunctionCall) otto.Value {
-		return *def_openfile(js, &call, dir, os.O_RDWR|os.O_CREATE|os.O_TRUNC)
+	obj.Set("create", func(filename string) otto.Value {
+		return *def_openfile(js, filepath.Join(dir, filename), os.O_RDWR|os.O_CREATE|os.O_TRUNC)
 	})
-	obj.Set("mkdir", func(call otto.FunctionCall) otto.Value {
-		return *def_invokefunc_error(js, call, func(s string) error { return os.Mkdir(s, 0666) })
+	obj.Set("mkdir", func(dir string) otto.Value {
+		return *js.CreateError(os.Mkdir(dir, 0666))
 	})
-	obj.Set("mkdirall", func(call otto.FunctionCall) otto.Value {
-		return *def_invokefunc_error(js, call, func(s string) error { return os.MkdirAll(s, 0666) })
+	obj.Set("mkdirall", func(dir string) otto.Value {
+		return *js.CreateError(os.MkdirAll(dir, 0666))
 	})
-	obj.Set("remove", func(call otto.FunctionCall) otto.Value {
-		return *def_invokefunc_error(js, call, os.Remove)
+	obj.Set("remove", func(file string) otto.Value {
+		return *js.CreateError(os.Remove(file))
 	})
-	obj.Set("removeall", func(call otto.FunctionCall) otto.Value {
-		return *def_invokefunc_error(js, call, os.RemoveAll)
+	obj.Set("removeall", func(file string) otto.Value {
+		return *js.CreateError(os.RemoveAll(file))
 	})
-	obj.Set("readdir", func(call otto.FunctionCall) otto.Value {
-		return *def_invokefunc_value(call, func(dirname string) *otto.Value {
-			fis, err := ioutil.ReadDir(dirname)
-			if err != nil {
-				return js.CreateError(err)
-			}
-			val := js.CreateArray()
-			for _, fi := range fis {
-				val.Object().Call("push", fi.Name())
-			}
-			return val
-		})
+	obj.Set("readdir", func(dirname string) otto.Value {
+		fis, err := ioutil.ReadDir(dirname)
+		if err != nil {
+			return *js.CreateError(err)
+		}
+		fs := make([]string, len(fis))
+		for i := range fis {
+			fs[i] = fis[i].Name()
+		}
+		return *js.CreateAny(fs)
 	})
 	return obj
 }
 
 // get jssp.file object
-func def_openfile(js *JavaScript, call *otto.FunctionCall, dir string, flag int) *otto.Value {
-	p := call.Argument(0)
-	if p.IsUndefined() {
-		return &p
-	}
-	name := filepath.Join(dir, p.String())
-	return def_openfilebyname(js, name, flag)
-}
-
-// get jssp.file object
-func def_openfilebyname(js *JavaScript, name string, flag int) *otto.Value {
+func def_openfile(js *JavaScript, name string, flag int) *otto.Value {
 	f, err := os.OpenFile(name, flag, 0666)
 	if err != nil {
 		return js.CreateError(err)
 	}
 	val := js.CreateObjectValue()
 	obj := val.Object()
-	obj.Set("write", func(call otto.FunctionCall) otto.Value {
-		return *def_invokefunc_error(js, call, func(s string) error {
-			_, err := f.WriteString(s)
-			return err
-		})
+	obj.Set("write", func(str string) otto.Value {
+		_, err := f.WriteString(str)
+		return *js.CreateError(err)
 	})
-	obj.Set("read", func(call otto.FunctionCall) otto.Value {
+	obj.Set("read", func() otto.Value {
 		data, err := ioutil.ReadAll(f)
 		if err != nil {
 			return *js.CreateError(err)
 		}
 		return *js.CreateAny(string(data))
 	})
-	obj.Set("info", func(call otto.FunctionCall) otto.Value {
+	obj.Set("info", func() otto.Value {
 		return *build_fileinfo(js, f)
 	})
-	obj.Set("parent", func(call otto.FunctionCall) otto.Value {
-		return *js.CreateAny(path.Base(name))
+	obj.Set("parent", func() string {
+		return path.Base(name)
 	})
-	obj.Set("close", func(call otto.FunctionCall) otto.Value {
+	obj.Set("close", func() otto.Value {
 		return *js.CreateError(f.Close())
 	})
-	obj.Set("move", func(call otto.FunctionCall) otto.Value {
-		return *def_invokefunc_error(js, call, func(newName string) error {
-			err := os.Rename(name, newName)
-			if err == nil {
-				name = newName
-			}
-			return err
-		})
+	obj.Set("move", func(newName string) otto.Value {
+		err := os.Rename(name, newName)
+		if err == nil {
+			name = newName
+		}
+		return *js.CreateError(err)
 	})
 	return val
-}
-
-// execute (func(string) error) by js calling the parameter
-func def_invokefunc_error(js *JavaScript, call otto.FunctionCall, fun func(string) error) *otto.Value {
-	p := call.Argument(0)
-	if p.IsUndefined() {
-		return &p
-	}
-	return js.CreateError(fun(p.String()))
-}
-
-// execute (func(string) *otto.Value) by js calling the parameter
-func def_invokefunc_value(call otto.FunctionCall, fun func(string) *otto.Value) *otto.Value {
-	p := call.Argument(0)
-	if p.IsUndefined() {
-		return &p
-	}
-	return fun(p.String())
 }
 
 // build jssp.fileinfo by os.FileInfo
 func build_fileinfo(js *JavaScript, f *os.File) *otto.Value {
 	fi, err := f.Stat()
 	if err != nil {
-		return js.CreateError(err)
+		panic(err)
 	}
 	val := js.CreateObjectValue()
 	obj := val.Object()
-	obj.Set("name", func(call otto.FunctionCall) otto.Value {
-		return *js.CreateAny(fi.Name())
-	})
-	obj.Set("size", func(call otto.FunctionCall) otto.Value {
-		return *js.CreateAny(fi.Size())
-	})
-	obj.Set("mode", func(call otto.FunctionCall) otto.Value {
-		return *js.CreateAny(fi.Mode().String())
-	})
-	obj.Set("time", func(call otto.FunctionCall) otto.Value {
-		return *js.CreateAny(fi.ModTime())
-	})
-	obj.Set("isdir", func(call otto.FunctionCall) otto.Value {
-		return *js.CreateAny(fi.IsDir())
-	})
+	obj.Set("name", fi.Name)
+	obj.Set("size", fi.Size)
+	obj.Set("mode", fi.Mode().String)
+	obj.Set("time", fi.ModTime)
+	obj.Set("isdir", fi.IsDir)
 	return val
 }
